96ab3d21683d21349e50b8f742550ef84be7bab8
Md-AbdullahAl-Noman <abdullahnoman1414@gmail.com>
2026-04-24 20:30:23 +0200

    Add initial go.mod and main.go files with packageone implementation

---
+++ scope/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"myapp/packageone"
+)
+
+
+var one="One"//global variable scoped to the package main
+func main(){
+	
+    var one="This one variable is block level variable and from main function scope that shadows the global variable one"// block level variable that shadows the global variable one
+	fmt.Println(one)
+	myFunc()
+	newString:=packageone.PublicVar//accessing the public variable from packageone
+	fmt.Println(newString)
+	
+	secondSring:=packageone.PrivateVar// trying to access the private variable from packageone will result in an error because it is not exported and only accessible within the packageone
+	fmt.Println(secondSring)
+}
+
+func myFunc(){
+	// var one="Number one"// function scope variable that shadows the global variable one
+	fmt.Println(one)
+}
---
+++ scope/packageone/package.go
@@ -0,0 +1,4 @@
+package packageone
+
+var privateVar="I am private"
+var PublicVar="I am public or exported"//the its PascalCase that makes it public and accessible from other packages
